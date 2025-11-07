d374f3eea26ba5c3ac5570d9222972e7acde2566
Manas Singh <manasinghofficial@gmail.com>
2025-11-07 10:40:56 +0530

    fix: update token validation logic in refreshAccessToken and verifyJWT middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -172,10 +172,11 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
+
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request");
   }
 
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,10 +9,12 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token =
       req.cookies?.accesstoken ||
       req.header("Authorization")?.replace("Bearer ", "");
+
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
     const user = await User.findById(decodedToken._id).select(
       "-password -refreshToken"
     );
